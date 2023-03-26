6f52bf38dd955556a31d53fff6bde7c12ab849e8
E6L1PS <danils003@mail.ru>
2023-03-26 17:28:39 +0300

    Added dependencies and bottom navigation menu

---
+++ app/src/main/java/com/itacademy/waceplare/ui/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.itacademy.waceplare.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.itacademy.waceplare.R
+import com.itacademy.waceplare.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navView: BottomNavigationView = binding.bnv
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_search,
+                R.id.navigation_favourites,
+                R.id.navigation_ads,
+                R.id.navigation_messages,
+                R.id.navigation_profile
+            )
+        )
+
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/itacademy/waceplare/ui/fragments/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.itacademy.waceplare.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.itacademy.waceplare.R
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
